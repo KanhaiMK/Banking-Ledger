99a80c9416fbe25c51bf086db362dc0a0b56e138
Kanhai Shaw <shawkanhai0@gmail.com>
2026-06-19 18:52:57 +0530

    email done

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,43 +1,25 @@
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    family: 4,  // force IPv4
-    auth: {
-        type: 'OAuth2',
-        user: process.env.EMAIL_USER,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-    },
-});
-
-// Verify the connection configuration
-transporter.verify((error, success) => {
-    if (error) {
-    console.error('Error connecting to email server:', error);
-    } else {
-    console.log('Email server is ready to send messages');
-    }
-});
+const { Resend } = require('resend');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 // Function to send email
 const sendEmail = async (to, subject, text, html) => {
     try {
-    const info = await transporter.sendMail({
-      from: `"MK_Bank" <${process.env.EMAIL_USER}>`, // sender address
-      to, // list of receivers
-      subject, // Subject line
-      text, // plain text body
-      html, // html body
-    });
+        const { data, error } = await resend.emails.send({
+            from: 'MK_BANK <onboarding@resend.dev>', // use this until you verify your own domain
+            to,
+            subject,
+            text,
+            html,
+        });
 
-    console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        if (error) {
+            console.error('Error sending email:', error);
+            return;
+        }
+
+        console.log('Message sent:', data.id);
     } catch (error) {
-    console.error('Error sending email:', error);
+        console.error('Error sending email:', error);
     }
 };
 
